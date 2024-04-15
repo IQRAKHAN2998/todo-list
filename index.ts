a3730e288f85cb2b11990cade8a37432272c886d
IQRAKHAN2998 <160261671+IQRAKHAN2998@users.noreply.github.com>
2024-04-15 23:44:08 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+let todos =[];
+let condition = true ;
+while(condition)
+    {
+        let todoQuestions = await inquirer.prompt(
+        [
+            {
+                name : "todoList",
+                type : "input",
+                message : "what would you like to add in you todos?"
+            },
+            {
+                 name : "addMore",
+                 type : "confirm",
+                 message : "would you like to add more in your todos?",
+                default : "yes"
+            }
+        ]
+    );
+
+
+todos.push(todoQuestions.todoList);
+condition = todoQuestions.addMore
+    console.log(todos)
+    }
